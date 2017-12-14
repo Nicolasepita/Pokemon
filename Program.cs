07fac292c112ed46c3a09231c973f08fd03ee32b
Nicolasepita <nicolas1.marie@epita.fr>
2017-12-14 17:24:29 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pokemon
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
