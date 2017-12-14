6f5c0271dbf89f73be95f545e9dbbdc84c119d44
Nicolasepita <nicolas1.marie@epita.fr>
2017-12-14 23:53:45 +0100

    fisrt real commit

---
+++ Animal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Pokemon
+{
+    public class Animal
+    {
+        private string name;
+        
+        public Animal(string name)
+        {
+            this.name = name;
+        }
+        
+        public string Name => name;
+
+        public virtual void WhoAmI()
+        {
+            Console.WriteLine("I am an animal !");
+        }
+
+        public virtual void Describe()
+        {
+            Console.WriteLine("My name is " + name + ".");
+        }
+
+        public void Rename(string NewName)
+        {
+            name = NewName;
+        }        
+    }
+}
