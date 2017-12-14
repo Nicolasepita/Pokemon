83c01c204ac7b9b62742746e82749d2dd7e47660
qw2100m <martin.schmidt@epita.fr>
2017-12-14 22:46:25 +0100

    Class used to gather infos about pokemons from pokeapi.com

---
+++ Pokedex.cs
@@ -0,0 +1,79 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System;
+using System.Runtime.Remoting.Metadata;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using LitJson;
+
+namespace miniPokemon
+{
+    public class Pokedex
+    {
+        private enum API_STATS 
+        {
+            SPEED,
+            SPEC_DEF,
+            SPEC_ATK,
+            DEF,
+            ATK,
+            HP
+        }
+        
+        private const string WIKI_URL = "http://pokeapi.co/api/v2/pokemon/";
+        private const string API_404 = "{\"detail\":\"Not found.\"}";
+
+        private static XPokemon.XPoketype StrToType(string s)
+        {
+            switch (s)
+            {
+                case "POISIN":
+                    return XPokemon.XPoketype.FIRE;
+                case "FIRE":
+                    return XPokemon.XPoketype.FIRE;
+                case "WATER":
+                    return XPokemon.XPoketype.WATER;
+                case "GRASS":
+                    return XPokemon.XPoketype.GRASS;
+                case "ELECTRIC":
+                    return XPokemon.XPoketype.ELECTRIC;
+                default:
+                    return XPokemon.XPoketype.OTHER;
+            }
+        }
+        
+        public async Task GetPokemon(string name, XPokemon pk)
+        {
+            HttpClient hc = new HttpClient();
+            Console.WriteLine("Querying {0}'s info...", name);
+            HttpResponseMessage response = await hc.GetAsync(WIKI_URL + name.ToLower());
+            string responseString = await response.Content.ReadAsStringAsync();
+
+            if (responseString == API_404)
+            {
+                Console.WriteLine("Woah, looks like {0} doesn't exist !", name);
+                return;
+            }
+
+            /* Parse the API's data */
+            
+            JsonData api_data = JsonMapper.ToObject(responseString);
+            
+            /* Rename the pokemon (name defaults to "not found". */
+            pk.Rename(api_data["name"].ToString());
+
+            string hp = api_data["stats"][(int) API_STATS.HP]["base_stat"].ToString();
+            pk.Life = int.Parse(hp);
+
+            string speed = api_data["stats"][(int) API_STATS.SPEED]["base_stat"].ToString();
+            
+            
+            /* HARDCODED: Get the pokemon's first type. */
+            string type = api_data["types"][0]["type"]["name"].ToString().ToUpper();
+            pk.Poketype = StrToType(type);
+            
+            return;
+        }
+    }
+}
